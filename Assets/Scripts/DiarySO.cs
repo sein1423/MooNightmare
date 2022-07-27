149ae6f8a314d321eb43b6fd20cee82cfa2ca8b0
sein1423 <kun3650@gmail.com>
2022-07-27 23:55:23 +0900

    그림일기 데이터 추가

---
+++ Assets/Scripts/DiarySO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/DiarySO")]
+public class DiarySO : ScriptableObject
+{
+    public Sprite diaryImage;
+    public string diaryTitle;
+    public string weather;
+    public string feel;
+    [TextArea]
+    public string diaryEx;
+}
--- Assets/Scripts/FriendManager.cs
+++ Assets/Scripts/FriendManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class FriendManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject Dream;
+    GameObject DreamPanel;
     [SerializeField]
-    GameObject Diary;
+    GameObject DiaryPanel;
+    [SerializeField]
+    Text CarrotText;
+
+    [SerializeField]
+    Image DiaryImage;
+    [SerializeField]
+    Text DiaryTitle;
+    [SerializeField]
+    Text DiaryWeather;
+    [SerializeField]
+    Text Diaryfeel;
+    [SerializeField]
+    Text DiaryText;
+
+    [SerializeField]
+    DiarySO[] diary;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CarrotText.text = GameManager.Instance.user.carrot.ToString();
+        UpdateDiary(0);
     }
 
     // Update is called once per frame
@@ -28,21 +48,31 @@ public class FriendManager : MonoBehaviour
 
     public void LookDream()
     {
-        if (Dream.activeSelf)
+        if (DreamPanel.activeSelf)
         {
             return;
         }
-        Diary.SetActive(false);
-        Dream.SetActive(true);
+        DiaryPanel.SetActive(false);
+        DreamPanel.SetActive(true);
     }
 
     public void LookDiary()
     {
-        if (Diary.activeSelf)
+        if (DiaryPanel.activeSelf)
         {
             return;
         }
-        Dream.SetActive(false);
-        Diary.SetActive(true);
+        DreamPanel.SetActive(false);
+        DiaryPanel.SetActive(true);
+    }
+
+    public void UpdateDiary(int a)
+    {
+        diary[a].diaryEx = diary[a].diaryEx.Replace("Username", GameManager.Instance.user.name);
+        DiaryImage.sprite = diary[a].diaryImage;
+        DiaryTitle.text = "제목 : " + diary[a].diaryTitle;
+        DiaryWeather.text = "날씨 : " + diary[a].weather;
+        Diaryfeel.text = "기분 : " + diary[a].feel;
+        DiaryText.text = diary[a].diaryEx;
     }
 }
